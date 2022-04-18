8e9bf9f494e126e7934f34342703e775f78e8243
Aster <192608617@qq.com>
2022-04-18 15:27:43 +0800

    Parse sudoku from string

--- projects/deus-sudoku/src/lib.rs
+++ projects/deus-sudoku/src/lib.rs
@@ -1,1 +1,2 @@
+mod mini;
 mod standard;
--- projects/deus-sudoku/src/standard/mod.rs
+++ projects/deus-sudoku/src/standard/mod.rs
@@ -1,30 +1,106 @@
+use std::{convert::TryInto, str::FromStr};
 
+mod display;
+mod parse;
 
+pub struct Sudoku {}
 
-fn is_solvable() {
+impl Sudoku {}
 
+#[derive(Clone)]
+pub struct SudokuBoard {
+    rank: usize,
+    state: Vec<usize>,
 }
 
-fn is_uniquely_solvable() {}
+impl Sudoku {
+    pub fn is_solvable<G>(game: G) -> bool
+    where
+        G: Into<SudokuBoard>,
+    {
+        game.into().is_solvable()
+    }
 
-
-#[test]
-fn test() {
-
-
-// Sudokus can be created from &str's in both block or line formats or directly from bytes.
-// here, an example in line format
-    let sudoku_line = "...2...633....54.1..1..398........9....538....3........263..5..5.37....847...1...";
-
-    let sudoku = sudoku::Sudoku::from_str_line(sudoku_line).unwrap();
-
-// Solve, print or convert the sudoku to another format
-    if let Some(solution) = sudoku.solve_unique() {
-        // print the solution in line format
-        println!("{}", solution);
-
-        // or return it as a byte array
-        let cell_contents: [u8; 81] = solution.to_bytes();
+    pub fn is_uniquely_solvable<G>(game: G) -> bool
+    where
+        G: Into<SudokuBoard>,
+    {
+        game.into().is_uniquely_solvable()
     }
 }
 
+impl SudokuBoard {
+    pub(crate) fn filled(&self) -> Vec<usize> {
+        self.state.iter().chain([0usize].iter().cycle()).take(self.rank.pow(4)).collect()
+    }
+    pub(crate) fn filled_u8(&self) -> Vec<u8> {
+        self.state.iter().map(|x| *x as u8).chain([0u8].iter().cycle()).take(self.rank.pow(4)).collect()
+    }
+}
+
+impl SudokuBoard {
+    pub fn new(v: Vec<usize>) -> Self {
+        let rank = v.len().sqrt().sqrt();
+        assert_eq!(rank.pow(4), v.len());
+        Self { rank, state: v }
+    }
+    pub fn new_with_rank(v: Vec<usize>, rank: usize) -> Self {
+        Self { rank, state: v }
+    }
+}
+
+impl SudokuBoard {
+    pub fn is_solvable(&self) -> bool {
+        match self.rank {
+            3 => {
+                let array: [u8; 81] = self.filled_u8().try_into().unwrap();
+                let mut solver = sudoku::Sudoku(array);
+                solver.solve_one().is_some()
+            }
+            _ => unimplemented!(),
+        }
+    }
+    pub fn is_unique_solvable(&self) -> bool {
+        match self.rank {
+            3 => {
+                let array: [u8; 81] = self.filled_u8().try_into().unwrap();
+                let mut solver = sudoku::Sudoku(array);
+                solver.is_uniquely_solvable()
+            }
+            _ => unimplemented!(),
+        }
+    }
+}
+
+pub fn generate(rank: usize) -> SudokuBoard {
+    SudokuBoard { rank, state: vec![] }
+}
+
+impl FromStr for SudokuBoard {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut out = vec![];
+        for c in s.as_bytes() {
+            let n = match c {
+                b'.' | b'*' | b'-' | b'_' => 0,
+                b'0'...b'9' => c - b'0',
+                b'a'...b'z' => c - b'a' + 10,
+                b'A'...b'Z' => c - b'A' + 10,
+                _ => return Err("invalid character"),
+            };
+            out.push(n as usize);
+        }
+        Ok(SudokuBoard::new(out))
+    }
+}
+
+#[test]
+fn test() {
+    let sudoku_line = "...2...633....54.1..1..398........9....538....3........263..5..5.37....847...1...";
+
+    let sudoku = SudokuBoard::from_str(sudoku_line).unwrap();
+
+    println!("{}", sudoku.is_solvable());
+    println!("{}", sudoku.is_unique_solvable());
+}
