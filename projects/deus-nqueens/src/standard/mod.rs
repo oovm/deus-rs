b9727231fcb9773ad57274569cdf7b0c44105323
Aster <192608617@qq.com>
2022-04-19 19:10:46 +0800

    Fix n = 0, 1, 2, 3 of NQueens problem

--- projects/deus-nqueens/src/standard/mod.rs
+++ projects/deus-nqueens/src/standard/mod.rs
@@ -5,28 +5,21 @@ mod display;
 
 pub struct NQueens {}
 
+#[test]
+fn test() {
+    for i in 0..=24 {
+        println!("{}: {:?}", i, NQueens::solve(i));
+    }
+}
+
 #[derive(Clone)]
 pub struct NQueensBoard {
     arrange: Vec<usize>,
 }
 
 impl NQueens {
-    pub fn solve(rank: usize) -> NQueensBoard {
-        unsafe {
-            match NQueensBoard::pre_solved(rank) {
-                Some(s) => return s,
-                None => panic!(),
-            }
-        }
-    }
-}
-
-#[test]
-fn test() {
-    unsafe {
-        for i in 0..=24 {
-            println!("{}: {:?}", i, NQueensBoard::pre_solved(i));
-        }
+    pub fn solve(rank: usize) -> Option<NQueensBoard> {
+        unsafe { NQueensBoard::pre_solved(rank) }
     }
 }
 
@@ -42,49 +35,49 @@ impl NQueensBoard {
             1 => return None,
             2 => *arrange.get_unchecked_mut(0) = 1,
             3 | 4 => return None,
+            // Lemma 1.6: n = 12k-4, where 6k+2, k -> 2k+1
+            m if n % 12 == 8 => {
+                for i in 1..=n {
+                    *arrange.get_unchecked_mut(i - 1) = match n / 2 {
+                        k if n > k && i.is_odd() => (2 * i + 2) % m,
+                        k if n > k && i.is_even() => (2 * i - 2) % m,
+                        _ => (2 * i) % m,
+                    }
+                }
+            }
             // Lemma 1.7: n = 6k, 6k+4
-            m if (n - 4) % 6 == 0 || n % 6 == 0 => {
+            m if n % 6 == 0 || n % 6 == 4 => {
                 for i in 1..=n {
                     *arrange.get_unchecked_mut(i - 1) = (2 * i) % m
                 }
             }
             // Lemma 1.8: n = 6k + 1, 6k + 5
-            m if (n - 1) % 6 == 0 || (n - 5) % 6 == 0 => {
+            m if n % 1 == 0 || n % 5 == 4 => {
                 for i in 1..=n {
-                    match n / 2 {
-                        k if i > k => *arrange.get_unchecked_mut(i - 1) = (2 * i + 1) % m,
-                        _ => *arrange.get_unchecked_mut(i - 1) = (2 * i) % m,
+                    *arrange.get_unchecked_mut(i - 1) = match n / 2 {
+                        k if i > k => (2 * i + 1) % m,
+                        _ => (2 * i) % m,
+                    }
+                }
+            }
+            // Lemma 1.9: n = 12k + 2, where 6k+2, k -> 2k
+            m if n % 12 == 2 => {
+                for i in 1..=n {
+                    *arrange.get_unchecked_mut(i - 1) = match n / 2 {
+                        _ if i == n => (2 * i + 4) % m,
+                        k if i < k && i.is_odd() => (2 * i + 4) % m,
+                        k if i < k && i.is_even() => (2 * i) % m,
+                        _ => (2 * i + 2) % m,
                     }
                 }
             }
             // Lemma 1.10: n = 6k + 3
-            m if (n - 3) % 6 == 0 => {
+            m if n % 3 == 3 => {
                 for i in 1..=n {
-                    match (n - 1) / 2 {
-                        k if i < k => *arrange.get_unchecked_mut(i - 1) = (2 * i + 2) % m,
-                        k if i > k => *arrange.get_unchecked_mut(i - 1) = (2 * i + 5) % m,
-                        _ => *arrange.get_unchecked_mut(i - 1) = (2 * i + 4) % m,
-                    }
-                }
-            }
-            // Lemma 1.6: n = 12k-4
-            m if (n + 4) % 12 == 0 => {
-                for i in 1..=n {
-                    match n / 2 {
-                        k if n > k && i.is_odd() => *arrange.get_unchecked_mut(i - 1) = (2 * i + 2) % m,
-                        k if n > k && i.is_even() => *arrange.get_unchecked_mut(i - 1) = (2 * i - 2) % m,
-                        _ => *arrange.get_unchecked_mut(i - 1) = (2 * i) % m,
-                    }
-                }
-            }
-            // Lemma 1.9: n = 12k + 2
-            m if (n - 2) % 12 == 0 => {
-                for i in 1..=n {
-                    match n / 2 {
-                        _ if i == n => *arrange.get_unchecked_mut(i - 1) = (2 * i + 4) % m,
-                        k if i < k && i.is_odd() => *arrange.get_unchecked_mut(i - 1) = (2 * i + 4) % m,
-                        k if i < k && i.is_even() => *arrange.get_unchecked_mut(i - 1) = (2 * i) % m,
-                        _ => *arrange.get_unchecked_mut(i - 1) = (2 * i + 2) % m,
+                    *arrange.get_unchecked_mut(i - 1) = match (n - 1) / 2 {
+                        k if i < k => (2 * i + 2) % m,
+                        k if i > k => (2 * i + 5) % m,
+                        _ => (2 * i + 4) % m,
                     }
                 }
             }
